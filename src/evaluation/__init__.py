236cd765f11520ef615fb238863283ff11096d9e
mikailachmad <mikailkuliah@gmail.com>
2026-06-03 09:42:07 +0700

    update mikail's progress

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,9 +1,7 @@
 # src/evaluation/__init__.py
-from .kg_evaluator import KGEvaluator, KGStats
-from .kg_visualizer import generate_all_visualizations
+from .pre_kg_evaluator import KGEvaluator, KGStats
+from .pre_kg_visualizer import generate_all
+from .report_generator import generate_report
+from .run_evaluation import run
 
-__all__ = [
-    "KGEvaluator",
-    "KGStats",
-    "generate_all_visualizations",
-]
+__all__ = ["KGEvaluator", "KGStats", "generate_all", "generate_report", "run"]
